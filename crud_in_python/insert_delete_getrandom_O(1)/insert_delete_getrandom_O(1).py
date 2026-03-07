d0408b760b26a728468b7fd7b141cd8e3da3decc
Brendan <tobrendanw@gmail.com>
2026-03-07 09:18:12 -0800

    update leetcode crud

--- crud_in_python/insert_delete_getrandom_O(1)/insert_delete_getrandom_O(1).py
+++ crud_in_python/insert_delete_getrandom_O(1)/insert_delete_getrandom_O(1).py
@@ -65,3 +65,48 @@ class RandomizedSet:
 
     def getRandom(self) -> int:
         return random.choice(self.arr)
+    
+# my version of O(1)
+
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.store = []
+        self.indices = {}
+
+    def insert(self, val: int) -> bool:
+        if val not in self.indices:
+            self.store.append(val)
+            self.indices[val] = len(self.store) - 1
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        if val in self.indices:
+            # locate index of element to be removed
+            remove_index = self.indices[val]
+            # determine last value of list
+            last_value = self.store[-1]
+            # set to be removed element as last element
+            self.store[remove_index] = last_value
+            # fix dict index tracking
+            self.indices[last_value] = remove_index
+            # remove last element
+            self.store.pop()
+            # delete removed value from dict
+            self.indices.pop(val)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        x = random.randint(0, len(self.store) - 1)
+        return self.store[x]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
